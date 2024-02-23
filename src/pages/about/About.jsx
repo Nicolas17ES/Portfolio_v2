8f57576af2698b6aef646ff00fad51c3ce98ff99
Nicolas17ES <83025990+Nicolas17ES@users.noreply.github.com>
2024-02-23 16:45:18 +0100

    scrolltrigger solved

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -49,30 +49,30 @@ function About() {
    useSubtitleAnimation(display_body, inView1, ".about-subtitle-1");
    useSubtitleAnimation(display_body, inView2, ".about-subtitle-2");
 
-
    useGSAP(() => {
       if (display_body) {
-         const titles = gsap.utils.toArray('.para');
+         const titles = gsap.utils.toArray('.about-paragraph');
+         console.log("text", titles)
          titles.forEach(title => {
             const text = new SplitTextJS(title, { type: "words" });
-            console.log("text", text)
             gsap.from(text.chars, {
                scrollTrigger: {
                   trigger: title,
                   start: 'top 80%',
                   end: 'top 20%',
                   scrub: true,
-                  markers: true
+                  markers: false
                },
                opacity: 0.2,
-               stagger: 0.1
+               stagger: 0.1,
+               duration: 1,
+               ease: 'back.out',
             })
          });
       }
    }, [display_body]);
 
- 
-
+  
 
    useGSAP(() => {
       if(display_body){
@@ -100,7 +100,7 @@ function About() {
                   opacity: 1,
                   y: 0, // Move letters up a bit more
                   duration: .5,
-                  stagger: 0.07,
+                  stagger: 0.06,
                   ease: 'power2',               
                   // Apply bounce easing
                }, "<1"); // Add to the end of the previous animation
@@ -116,15 +116,22 @@ function About() {
         <div ref={bodyRef} className="body-container">
             <section className="about-image-container">
                
-               <h2 className="about-title">Nicolas</h2>
-               <h2 className="about-title">Luque</h2>
-               <h2 className="about-title">Rodriguez</h2>
+               <div className="about-title-container">
+                  <h2 className="about-title">Nicolas</h2>
+                  <span className="about-title-word">Developer</span>
+               </div>
+               <div className="about-title-container">
+                  <span className="about-title-word">Lover</span>
+                  <h2 className="about-title">Luque</h2>
+               </div>
+               <div className="about-title-container">
+                  <h2 className="about-title">Rodriguez</h2>
+                  <span className="about-title-word">Sexy Bitch</span>
+               </div>
             </section>
             <section className="about-content-container">   
                <h3 ref={refSubtitle1} className={`about-subtitle about-subtitle-1 ${inView1 ? 'in-view' : 'not-in-view'}`}>Behind the Keyboard</h3>
                <p ref={refParagraph1} className="about-paragraph">I'm a <span className="red">frontend</span> and <span className="red">backend</span> developer dedicated to crafting digital experiences that resonate. With a passion for innovation and a keen eye for detail, I bring  <span className="red">ideas to life</span>  in pixels and code.</p>
-                              <p className="about-paragraph para">I specialize in bridging the gap between frontend elegance and backend brilliance. From crafting seamless  to architecting robust systems, I ensure that every is as functional as it is beautiful.</p>
-
             </section>
             <section className="about-content-container">
                <h3 ref={refSubtitle2} className={`about-subtitle about-subtitle-2 ${inView2 ? 'in-view' : 'not-in-view'}`}>Embarking on Web Adventures</h3>
@@ -132,12 +139,6 @@ function About() {
             </section>
          </div> 
 
-         // <div className="left">
-         //          <h3 className="about-subtitle">Embarking on Web Adventures</h3>
-         //       <p className="about-paragraph">I specialize in bridging the gap between frontend elegance and backend brilliance. From crafting seamless user interfaces to architecting robust systems, I ensure that every digital creation is as functional as it is beautiful.</p>
-         //       </div>
-         //       <div className="right">
-         //       </div>
    );
   }
 }
